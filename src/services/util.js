5fc4eb9192bda9f17a0ff4cb314dacf7de2c7059
Nikosar <35628353+Nikosar@users.noreply.github.com>
2020-04-01 22:22:42 +0300

    Basic configuration creation

--- src/services/util.js
+++ src/services/util.js
@@ -1,3 +1,3 @@
 // export const CONFIGURATOR_API = "http://192.168.1.225:8080";
 // export const CONFIGURATOR_API = "http://localhost:8080";
-export const CONFIGURATOR_API = "hhtp://4281f58a.ngrok.io";
+export const CONFIGURATOR_API = "http://4281f58a.ngrok.io";
